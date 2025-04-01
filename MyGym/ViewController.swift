853e18046ce634c064c3c7d1b729ec9195ec1f72
Rad <radziod332@gmail.com>
2025-04-01 17:39:36 +0200

    Initial Commit

---
+++ MyGym/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGym
+//
+//  Created by Radoslaw on 01/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
